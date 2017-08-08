8425a103ce8d5b36c463ddb0cb8048d6cc6f8e66
A Q Khan <aqk.neo@gmail.om>
2017-08-08 05:20:34 +0500

    Coutdown timer

--- src/app/components/single-product/single-product.component.ts
+++ src/app/components/single-product/single-product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Router, ActivatedRoute, Params, NavigationEnd} from '@angular/router';
+import { Router, ActivatedRoute, Params, NavigationEnd } from '@angular/router';
 import { SingleProductService } from '../../services/single-product.service';
+import { Observable } from "rxjs/Rx";
+
 
 @Component({
   selector: 'app-single-product',
@@ -10,14 +12,27 @@ import { SingleProductService } from '../../services/single-product.service';
 
 export class SingleProductComponent implements OnInit {
   slug: string;
-  product: any = {
+  product: Product = {
     id: 1,
     name: 'Title',
     img: 'assets/images/oction-alt-white-1.png',
     price: 99,
-    date_time: '7/7/2017',
+    date_time: '2017-08-09',
     description: 'Loading description'
   };
+
+  public _trialEndsAt;
+
+  public _diff: number;
+
+  public _days: number;
+
+  public _hours: number;
+
+  public _minutes: number;
+
+  public _seconds: number;
+
   constructor(private activatedRoute: ActivatedRoute, private singleProduct: SingleProductService, private router: Router) {
     // subscribe to router event
     this.activatedRoute.params.subscribe((params: Params) => {
@@ -25,6 +40,7 @@ export class SingleProductComponent implements OnInit {
     });
     this.singleProduct.getSingleProduct(this.slug).subscribe((product) => {
       this.product = product;
+      this._trialEndsAt = this.product.date_time;
     });
   }
 
@@ -35,6 +51,32 @@ export class SingleProductComponent implements OnInit {
       }
       window.scrollTo(0, 0)
     });
+
+    Observable.interval(1000).map((x) => {
+      this._diff = Date.parse(this._trialEndsAt) - Date.parse(new Date().toString());
+    }).subscribe((x) => {
+      this._days = this.getDays(this._diff);
+      this._hours = this.getHours(this._diff);
+      this._minutes = this.getMinutes(this._diff);
+      this._seconds = this.getSeconds(this._diff);
+    });
+
+  }
+
+  getDays(t){
+    return Math.floor( t/(1000*60*60*24) );
+  }
+
+  getHours(t){
+    return Math.floor( (t/(1000*60*60)) % 24 );
+  }
+
+  getMinutes(t){
+    return Math.floor( (t/1000/60) % 60 );
+  }
+
+  getSeconds(t){
+    return Math.floor( (t/1000) % 60 );
   }
 
 }
